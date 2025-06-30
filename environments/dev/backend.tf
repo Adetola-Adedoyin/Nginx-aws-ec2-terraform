6b4bcc8ca18a3ac47107bab955b4358859be0eda
Adetola Adedoyin <108130465+Adetola-Adedoyin@users.noreply.github.com>
2025-06-30 14:18:45 +0100

    Update backend.tf

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "sdykbhzdkun"  
     key            = "terraform-state/dev/terraform.tfstate"
-    region         = "us-east-2"  
+    region         = "us-east-1"  
   }
 }
